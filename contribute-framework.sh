66ef12beb5b2026b033cfcec5d1357816fd34ff8
FISH <167963346+FISHSYET@users.noreply.github.com>
2024-05-30 11:55:42 +0800

    Delete contribute-framework.sh

--- contribute-framework.sh
+++
@@ -1,3 +0,0 @@
-cd bizui/src/ && cp -Ra common launcher permission components utils ~/githome/skynet-react-framework/src/
-cd ~/githome/skynet-react-framework/ &&git add ./&& git  commit -m "upgade framework" && git push
-
